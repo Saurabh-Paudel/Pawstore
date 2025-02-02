6b25532a5c94d8d97693c20eaf31afd0d91261be
Saurabh Paudel <saurabhpaudel2000@gmail.com>
2025-02-02 16:15:55 +0545

    newsletter section issue solved.

--- Client/src/Components/HomepageComponent/NewsletterSection.jsx
+++ Client/src/Components/HomepageComponent/NewsletterSection.jsx
@@ -3,14 +3,14 @@ import Dog from "../../assets/dog.png";
 
 export default function NewsletterSection() {
   return (
-    <div className="flex justify-center my-16 px-5 md:px-10 lg:px-10 xl:px-[141px]">
-      <div className="h-auto w-full bg-[#FFF0D9] rounded-[50px] px-8 py-5 lg:px-24 lg:py-2 flex flex-col lg:flex-row items-center justify-between">
+    <div className="flex justify-center my-16 px-5 md:px-10 lg:px-16 xl:px-[141px]">
+      <div className="h-auto w-full bg-[#FFF0D9] rounded-[50px] px-8 py-5 lg:px-24 lg:py-2 flex flex-col lg:flex-row items-center lg:items-start justify-between">
         {/* Image Section (Hidden on Small Screens) */}
         <div className="hidden lg:block">
           <img
             src={Dog}
             alt="Cute dog illustration"
-            className="max-h-[380px] lg:max-h-[430px] w-auto"
+            className="max-h-[380px] lg:max-h-[430px] w-full object-contain"
           />
         </div>
 
@@ -26,7 +26,7 @@ export default function NewsletterSection() {
             <input
               type="email"
               placeholder="Enter email..."
-              className="w-[400px] sm:max-w-[400px] h-[50px] px-4 rounded-[20px] bg-white text-gray-600 shadow-md focus:outline-none lg:h-[60px] lg:text-lg"
+              className="w-full max-w-[400px] h-[50px] px-4 rounded-[20px] bg-white text-gray-600 shadow-md focus:outline-none lg:h-[60px] lg:text-lg"
             />
             <button className="bg-[#E58608] hover:bg-[#D87407] transition-all duration-300 rounded-full font-medium text-lg h-[48px] w-[160px] text-white shadow-md lg:w-[180px] lg:h-[55px] lg:text-xl">
               Subscribe
